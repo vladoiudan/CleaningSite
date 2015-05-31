5d6143497c1bfe4d335977e434bcb5ddaf8c444b
vladoiudan <vladoiudan@yahoo.com>
2015-05-31 20:41:13 +0100

    combined scripts and styles

--- DAV.UniverseCleaning/DAV.UniverseCleaning/App_Start/BundleConfig.cs
+++ DAV.UniverseCleaning/DAV.UniverseCleaning/App_Start/BundleConfig.cs
@@ -9,20 +9,14 @@ namespace DAV.UniverseCleaning
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery-{version}.js",
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/excoloSlider").Include(
-                      "~/Scripts/jquery.excoloSlider.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                        "~/Scripts/modernizr-*",
+                      "~/Scripts/jquery.excoloSlider.js",
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
