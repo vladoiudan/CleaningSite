70d0efe50d34b83bce3f1249becd240499401a4b
vladoiudan <vladoiudan@yahoo.com>
2015-10-18 18:35:38 +0100

    improvement and removed unneeded nuget packages

---
+++ DAV.UniverseCleaning/DAV.UniverseCleaning/Models/TestimonialModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DAV.UniverseCleaning.Models
+{
+    public class TestimonialModel
+    {
+        public string PersonTitle { get; set; }
+        public string PersonName { get; set; }
+        public DateTime Date { get; set; }
+        public string Content { get; set; }
+        public string Location { get; set; }
+    }
+}
